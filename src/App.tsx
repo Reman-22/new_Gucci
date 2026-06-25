70a5b52bc8b04bec5a3c24d596f88a4290f09cb6
Reman-22 <reemanha0@gmail.com>
2026-06-25 15:18:02 -0700

    improved website

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { routes } from "./routes";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes ,{
+  basename:"/New_Gucci",
+});
 
 export default function App() {
   return (
